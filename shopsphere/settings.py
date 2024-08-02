8b069fb6c614d04e4c82087209cd47856bd9aafc
Ieshan Chaudhary <ieshan3132@gmail.com>
2024-08-02 20:44:23 +0530

    settings changes

--- shopsphere/settings.py
+++ shopsphere/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9nk#zjw!oiibde@x4!fx6(0%w41zauvw(7jvy4kof5ucqyl#6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'https://shopsphere.onrender.com', 'https://shopsphere-cezh.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'https://shopsphere-u1pr.onrender.com']
 
 
 # Application definition
